6c074d21d2a9f8d341b700d5ea979102ad46b604
Sohail Ali Khwazada <sohailali.khwazada2011@gmail.com>
2025-02-02 00:33:14 +0530

    noting major

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/",(req,res)=> {
   res.json({message: "Hello world"});
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT,()=> {
   connectDb();
   console.log(`Server is running on port ${PORT}`);
 })
